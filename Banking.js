a12250082cb4ea8784af9f3a37bbe248dd35e429
Hablu-p <ek12bd@gmail.com.com>
2022-10-19 13:57:25 +0600

    git correctin commit

--- Banking.js
+++ Banking.js
@@ -2,10 +2,12 @@ document.getElementById("button-deposit").addEventListener('click', function () 
     const inputDeposit = document.getElementById("input-deposit");
     const amountDepositText = inputDeposit.value;
     const amountDeposit = parseFloat(amountDepositText);
+
     const totalDeposit = document.getElementById('total-deposit');
     const totalDepositText =  totalDeposit.innerText;
     const previousTotalDeposit = parseFloat(totalDepositText);
     totalDeposit.innerText = previousTotalDeposit + amountDeposit;
+
     const totalBalance = document.getElementById('total-balance');
     const totalBalanceText =  totalBalance.innerText;
     const previousTotalBalance = parseFloat(totalBalanceText);
@@ -17,17 +19,19 @@ document.getElementById("button-withdrow").addEventListener('click', () => {
     const inputWithdrow = document.getElementById("input-withdrow");
     const amountWithdrowText = inputWithdrow.value;
     const amountWithdrow = parseFloat(amountWithdrowText);
+
     const totalWithdrow = document.getElementById('total-withdrow');
     const totalWithdrowText =  totalWithdrow.innerText;
     const previousTotalWithdrow = parseFloat(totalWithdrowText);
     totalWithdrow.innerText = previousTotalWithdrow + amountWithdrow;
+
     const totalBalance = document.getElementById('total-balance');
     const totalBalanceText =  totalBalance.innerText;
     const previousTotalBalance = parseFloat(totalBalanceText);
     totalBalance.innerText = previousTotalBalance - amountWithdrow;   
+    inputWithdrow.value = "";
 
-        inputWithdrow.value = "";
-    });
+});
 
 
     
